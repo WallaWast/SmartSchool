92e866bdabb094a6ef65622f7307752097f6a090
Wast <wallacewst@gmail.com>
2020-06-21 18:42:49 -0300

    Configuração newtonsoft

--- SmartSchool.Api/Startup.cs
+++ SmartSchool.Api/Startup.cs
@@ -33,7 +33,10 @@ namespace SmartSchool.Api
 
             services.AddScoped<IRepository, Repository>();
 
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(
+                        opt => opt.SerializerSettings.ReferenceLoopHandling =
+                            Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
